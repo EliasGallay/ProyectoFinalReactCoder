3fa7234ceee2ed94e9a93371c259db6be6792909
Elias Gallay <elias.gallay@beex.ar>
2025-05-18 18:44:41 -0300

    fix/align price in CustomCard

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -3,17 +3,20 @@ import { Card } from "antd";
 const CustomCard = ({ name, description, price }) => (
   <Card
     hoverable
-    className="w-60"
+    className="w-auto h-full flex flex-col justify-between"
     cover={
       <img
         alt="example"
         src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
       />
     }
+    bodyStyle={{ display: "flex", flexDirection: "column", height: "100%" }}
   >
-    <h3 className="text-lg font-bold mb-2 d-flex center">{name}</h3>
-    <p className="text-gray-600 mb-3">{description}</p>
-    <div className="mt-3 text-primary font-bold text-base">${price}</div>
+    <div className="flex-1">
+      <h3 className="text-lg font-bold mb-2">{name}</h3>
+      <p className="text-gray-600 mb-3">{description}</p>
+    </div>
+    <div className="text-primary font-bold text-base p-1 mt-auto">${price}</div>
   </Card>
 );
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import CustomCard from "./CustomCard";
 
 const ItemListContainer = ({ products }) => {
   return (
-    <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 bg-gray-100 mx-auto max-w-screen-xl">
+    <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 bg-gray-100 mt-5 rounded-md mx-auto max-w-screen-xl">
       {products.map((product) => (
         <CustomCard
           key={product.id}
