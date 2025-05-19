feb4ce0ce3877c4437ada4f512121b279920ab03
Elias Gallay <elias.gallay@beex.ar>
2025-05-18 21:05:27 -0300

    feat: add routing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <ItemListContainer />
-    </>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
