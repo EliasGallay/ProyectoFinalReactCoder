e4834440ee1445ff791f923c2578d20391c0653b
Elias Gallay <elias.gallay@beex.ar>
2025-05-26 17:37:11 -0300

    feat/ implement firebase

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,24 @@ import App from "./App.jsx";
 import CustomConfigProvider from "./components/CustomConfigProvider.jsx";
 import "@ant-design/v5-patch-for-react-19";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDR22FfTBUpdiLr8J97lFTyFJ9BDynbZpc",
+  authDomain: "reactcoder-3df3c.firebaseapp.com",
+  projectId: "reactcoder-3df3c",
+  storageBucket: "reactcoder-3df3c.firebasestorage.app",
+  messagingSenderId: "428304801142",
+  appId: "1:428304801142:web:6a2e8d17fa95142c0fee57",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 createRoot(document.getElementById("root")).render(
   <CustomConfigProvider>
     <App />
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,17 +1,27 @@
-export const getProducts = () => {
-  return new Promise((resolve, reject) => {
-    fetch("/mocks/products.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error al cargar los productos");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setTimeout(() => {
-          resolve(data.products);
-        }, 2000);
-      })
-      .catch((error) => reject(error));
-  });
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+
+export const getProducts = async (categoryId = null) => {
+  try {
+    const db = getFirestore();
+    const productsCollection = collection(db, "libros");
+    let q = productsCollection;
+    if (categoryId) {
+      q = query(productsCollection, where("category", "==", categoryId));
+    }
+    const querySnapshot = await getDocs(q);
+    const products = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return products;
+  } catch (error) {
+    console.error("Error al cargar los productos:", error);
+    throw new Error("No se pudieron cargar los productos");
+  }
 };
