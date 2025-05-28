8a9c5f88f6e6649410181828c5c4bcb338a7ebc9
Elias Gallay <elias.gallay@beex.ar>
2025-05-27 22:12:53 -0300

    feat/ change title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white p-4 text-black flex justify-between items-center shadow-md">
       <Link to="/">
-        <div className="text-lg font-bold">My Website</div>
+        <div className="text-lg font-bold">BookStore 📚</div>
       </Link>
       <div className="flex space-x-4 items-center">
         <DropdownMenu />
